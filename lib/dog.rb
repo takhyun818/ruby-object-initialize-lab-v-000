257fc5bc63fede696051f714f9520b9a31dd95b6
Tak H Kim <takhyun818@gmail.com>
2018-09-06 18:24:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,14 @@
 class Dog
-  def breed=(breed)
-      @breed = breed
-    end
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
 
-    def breed
-      @breed
-    end
-def name=(the_name_of_the_dog)
-  @name = the_name_of_the_dog
-end
+  def name
+    @name
+  end
 
-def name
-  @name
-end
+  def breed
+    @breed
+  end
 end
