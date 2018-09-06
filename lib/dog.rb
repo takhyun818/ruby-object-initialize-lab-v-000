1e001e1428aa48183f20eff164cf752b7b2cfee2
Tak H Kim <takhyun818@gmail.com>
2018-09-06 18:22:42 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  def breed=(breed)
+      @breed = breed
+    end
+
+    def breed
+      @breed
+    end
+def name=(the_name_of_the_dog)
+  @name = the_name_of_the_dog
+end
+
+def name
+  @name
+end
+end
